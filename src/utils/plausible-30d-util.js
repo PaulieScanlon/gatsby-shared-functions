fbdf217db9b1ffb5687f90edf97104cddb19f27e
pauliescanlon <paul.scanlon@gatsbyjs.com>
2022-09-22 11:38:27 +0100

    feat: add custom time period

--- src/utils/plausible-30d-util.js
+++ src/utils/plausible-30d-util.js
@@ -2,7 +2,7 @@ const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fet
 
 module.exports = async function () {
   try {
-    const response = await fetch('https://plausible.io/api/v1/stats/timeseries?site_id=paulie.dev&period=30d', {
+    const response = await fetch('https://plausible.io/api/v1/stats/timeseries?site_id=paulie.dev&period=custom&date=2022-08-23,2022-09-27', {
       headers: {
         Authorization: `Bearer ${process.env.PLAUSIBLE_API_KEY}`
       }
