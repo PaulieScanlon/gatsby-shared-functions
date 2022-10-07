2febae968e6098a04a07b77db540eb998420617c
pauliescanlon <paul.scanlon@gatsbyjs.com>
2022-10-07 08:19:45 +0100

    style: manually add classes to safelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,37 @@
 module.exports = {
   content: ['./src/pages/**/*.{js,jsx,ts,tsx}', './src/components/**/*.{js,jsx,ts,tsx}'],
-  safelist: [{ pattern: /from|to|text|stroke|fill|bg|ring|border-(sky|blue|pink|purple|lime|green)/, variants: ['peer-checked'] }],
+  safelist: [
+    'from-sky-400',
+    'from-pink-400',
+    'from-lime-400',
+    'to-blue-500',
+    'to-purple-500',
+    'to-lime-500',
+    'text-sky-400',
+    'text-pink-400',
+    'text-lime-400',
+    'text-sky-500',
+    'text-pink-500',
+    'text-lime-500',
+    'fill-sky-400',
+    'fill-pink-400',
+    'fill-lime-400',
+    'fill-sky-600',
+    'fill-pink-600',
+    'fill-lime-600',
+    'stroke-sky-400',
+    'stroke-pink-400',
+    'stroke-lime-400',
+    'stroke-sky-500',
+    'stroke-pink-500',
+    'stroke-lime-500',
+    'peer-checked:bg-sky-300',
+    'peer-checked:bg-pink-300',
+    'peer-checked:bg-lime-300',
+    'bg-sky-400',
+    'bg-pink-400',
+    'bg-lime-400'
+  ],
   theme: {
     extend: {
       colors: {
