4de032639ef1e330c97b926fa7740de6c60fdd19
pauliescanlon <paul.scanlon@gatsbyjs.com>
2022-10-20 12:18:05 +0100

    style: add min-w to fixed-dates

--- src/components/fixed-dates.js
+++ src/components/fixed-dates.js
@@ -10,7 +10,7 @@ const FixedDates = ({ start, end }) => {
     <div className="flex flex-cols gap-4 items-end justify-center xl:justify-start">
       <div className="flex flex-col gap-1">
         <small className="block font-bold text-slate-400 text-xs">Start</small>
-        <time className="inline-block m-0 text-slate-300 rounded border border-slate-200 px-2 select-none h-8 min-w-24">
+        <time className="inline-block m-0 text-slate-300 rounded border border-slate-200 px-2 select-none h-8 min-w-[104px]">
           <AppContext.Consumer>
             {({ hydrated }) => {
               return hydrated ? prettyDate(start) : '';
@@ -20,7 +20,7 @@ const FixedDates = ({ start, end }) => {
       </div>
       <div className="flex flex-col gap-1">
         <small className="block font-bold text-slate-400 text-xs">End</small>
-        <time className="inline-block m-0 text-slate-300 rounded border border-slate-200 px-2 select-none h-8 min-w-24">
+        <time className="inline-block m-0 text-slate-300 rounded border border-slate-200 px-2 select-none h-8 min-w-[104px]">
           <AppContext.Consumer>
             {({ hydrated }) => {
               return hydrated ? end : '';
